b2a41b815439f358b1bc76af6a517207cbc70435
Jazmin Rosario <jazminrosario@R2Hs-MacBook-Pro.local>
2023-05-02 16:15:07 -0400

    pusing codetalk

---
+++ apps.js
@@ -0,0 +1,5 @@
+const n = 5;   // unchangebale varriable 
+                     //i is index , i++ so it doesnt go on forever  
+for (let i = 1; i <= n; i++) {   //for indicates a for loop
+    console.log(`Jaz is awesomeness`);  
+}
